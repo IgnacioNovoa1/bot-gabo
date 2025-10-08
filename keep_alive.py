0f62a4427c37eda64201aa113ff90ea266c627f2
IgnacioNovoa1 <ignacionovoa8@gmail.com>
2025-10-08 00:14:09 -0300

    agrege main

--- keep_alive.py
+++ keep_alive.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from threading import Thread
 
@@ -8,7 +9,8 @@ def home():
     return "✅ El bot está corriendo", 200
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
